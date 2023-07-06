78c45b7da25f5d79b167e178c59eebf724851016
Krishan <krishangoyal9350@gmail.com>
2023-07-06 18:06:11 +0530

    Featured Products (Store)

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,24 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePgae = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("00d40fc2-1b31-4441-a9b3-a35e48c522c5");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HomePgae;
